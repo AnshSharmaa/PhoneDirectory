42b32362ee065b186d9aa23d5b370a62007c6ec4
Ansh Sharma <49720994+AnshSharmaa@users.noreply.github.com>
2021-04-23 10:13:40 +0530

    front end bindings,spring security,controllers

--- src/main/java/com/project/PhoneDir/controller/HomeController.java
+++ src/main/java/com/project/PhoneDir/controller/HomeController.java
@@ -10,14 +10,4 @@ public class HomeController {
     public String home(){
         return "home";
     }
-
-    @RequestMapping("/login")
-    public String login(){
-        return "login";
-    }
-
-    @RequestMapping("/signup")
-    public String signup(){
-        return "signup";
-    }
 }
